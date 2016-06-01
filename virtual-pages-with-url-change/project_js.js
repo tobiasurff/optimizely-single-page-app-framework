742e9aa804a2e8f59531e2b95f84c1815261c63b
Tobias Urff <turff@ams-turff1-4.local>
2016-06-01 16:13:42 +0200

    Added tracking for uncultured elements between activation and next mousedown

--- virtual-pages-with-url-change/project_js.js
+++ virtual-pages-with-url-change/project_js.js
@@ -1,32 +1,47 @@
 // Debug mode
 // Conditional statement
+debug_uncaptured = [];
+(function() {
+    if (window.MutationObserver || window.WebKitMutationObserver) {
+      var listeners = [],
+        doc = window.document,
+        MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+        observer,
+        last_activated_url = '',
+        debug_trackuncaptured = false;
+      document.onmousedown = function() {
+        debug_trackuncaptured = false;
+      }
 
-(function(debug =) {
-  if (window.MutationObserver || window.WebKitMutationObserver) {
-
-    var listeners = [],
-      doc = window.document,
-      MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-      observer;
-
-    var last_activated_url = '';
-
-    function check(elems) {
-      
-      console.log(elems);
-        
-          if (last_activated_url !== window.location.href && optimizely.$('body.loading').length < 1) {
-            
-            // Test change (would be activation call)
-            optimizely.$("head").append('<style>*{background:yellow;}</style>');
-            optimizely.$("*").css({"background":'#'+Math.floor(Math.random()*16777215).toString(16) });
-            
-            //window.optimizely.push(["activate", 6047861351]);
-            last_activated_url = window.location.href;
+      function check(elems) {
+        if (last_activated_url !== window.location.href && optimizely.$(
+          'body.loading').length < 1) {
+          // Test change (would be activation call)
+          //optimizely.$("head").append('<style>*{background:yellow;}</style>');
+          //optimizely.$("*").css({"background":'#'+Math.floor(Math.random()*16777215).toString(16) });
+          console.log(
+            '----------------------- ACTIVATED -----------------------');
+          debug_trackuncaptured = true;
+          //window.optimizely.push(["activate", 6047861351]);
+          last_activated_url = window.location.href;
+        } else if (debug_trackuncaptured == true) {
+          for (var i = 0, tot = debug_uncaptured.length; i < tot; i++) {
+            for (var io = 0, toto = debug_uncaptured[i].length; io < toto; io++) {
+              if (debug_uncaptured[i][io].addedNodes.length > 0) {
+                debug_uncaptured.push(debug_uncaptured[i][
+                  io
+                ].addedNodes));
+            }
           }
-      
+        }
+        debug_uncaptured.push(elems);
+        //console.log(debug_uncaptured);
+        //console.log(elems);
+        /*for (var i=0,  tot=myArray.length; i < tot; i++) {
+              console.log(myArray[i]); //"aa", "bb"
+            }*/
+      }
     }
-
     if (!observer) {
       // Watch for changes in the document
       observer = new MutationObserver(check);
@@ -35,13 +50,11 @@
         subtree: true
       });
     }
-
     // Tracking script usage
     window.optimizely = window.optimizely || [];
-  window.optimizely.push({
-    'type': 'integration',
-    'OAuthClientId': '6078211160'
-  });
-
+    window.optimizely.push({
+      'type': 'integration',
+      'OAuthClientId': '6078211160'
+    });
   }
-})(); 
+})();
