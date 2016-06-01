d30e73dfd166c71b41a9a9837694fd241230d804
Tobias Urff <turff@ams-turff1-4.local>
2016-06-01 14:27:56 +0200

    Added tracking snippet

--- virtual-pages-with-url-change/project_js.js
+++ virtual-pages-with-url-change/project_js.js
@@ -1,15 +1,7 @@
-String.prototype.hashCode = function() {
-  var hash = 0, i, chr, len;
-  if (this.length === 0) return hash;
-  for (i = 0, len = this.length; i < len; i++) {
-    chr   = this.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-};
+// Debug mode
+// Conditional statement
 
-(function() {
+(function(debug =) {
   if (window.MutationObserver || window.WebKitMutationObserver) {
 
     var listeners = [],
@@ -18,30 +10,20 @@ String.prototype.hashCode = function() {
       observer;
 
     var last_activated_url = '';
-    
-    var timer = [];
 
     function check(elems) {
       
       console.log(elems);
         
-        //window.clearTimeout(timer['aaaa']);
-        //timer['aaaa'] = window.setTimeout(function(){
-          if (last_activated_url !== window.location.href /*&& optimizely.$('body.loading').length < 1) {
+          if (last_activated_url !== window.location.href && optimizely.$('body.loading').length < 1) {
             
             // Test change (would be activation call)
             optimizely.$("head").append('<style>*{background:yellow;}</style>');
-            optimizely.$("*").css({
-          "background":'#'+Math.floor(Math.random()*16777215).toString(16) });
+            optimizely.$("*").css({"background":'#'+Math.floor(Math.random()*16777215).toString(16) });
             
             //window.optimizely.push(["activate", 6047861351]);
-            console.log('ACTIVATED');
             last_activated_url = window.location.href;
           }
-        //}, 1);
-        
-        
-
       
     }
 
@@ -53,6 +35,13 @@ String.prototype.hashCode = function() {
         subtree: true
       });
     }
+
+    // Tracking script usage
+    window.optimizely = window.optimizely || [];
+  window.optimizely.push({
+    'type': 'integration',
+    'OAuthClientId': '6078211160'
+  });
 
   }
 })(); 
