25610afe466143e5e5c7fefff742b016f84337c3
Tobias Urff <turff@ams-turff1-4.local>
2016-06-20 14:44:03 +0200

    Added debug mode (via parameter)

--- virtual-pages-with-url-change/project_js.js
+++ virtual-pages-with-url-change/project_js.js
@@ -3,20 +3,30 @@
     MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
     requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame,
     observer,
-    last_activated_url = '';
+    debug_mode = true || window.location.search.indexOf('optimizely_preview_activation') >= 0,
+    last_activated_url = '',
+    // Adapt condition to match your website (e.g. include waiting spinners and certain elements that have to be available before activation)
+    condition = function(){
+      return optimizely.$('.content-loading-indicator').css('display') != 'block' &&
+        optimizely.$('.content').length > 0 && 
+        optimizely.$('.logo-wrapper').length > 0;
+    };
 
   function check(elems) {
-    if (last_activated_url !== window.location.href && optimizely.$('.content-loading-indicator').css('display') != 'block' &&
-      optimizely.$('.content').length > 0 && optimizely.$('.logo-wrapper').length > 0) {
+    if (last_activated_url !== window.location.href && condition() ) {
       // Test change (would be activation call)
       requestAnimationFrame(function() {
         // Activate experiments via Manual Activation
         window.optimizely.push(["activate"]);
-        // Color change for debugging (yellow elements are inserted after activation, randomly colored elements are inserted before the activation)
-        optimizely.$("head").append('<style>*{background:yellow;}</style>');
-        optimizely.$("*").css({
-          "background": '#' + Math.floor(Math.random() * 16777215).toString(16)
-        });
+        if(debug_mode){
+          // Color change for debugging (yellow elements are inserted after activation, randomly colored elements are inserted before the activation)
+          optimizely.$("head").append('<style>*{background:yellow;}</style>');
+          optimizely.$("*").css({
+            "background": '#' + Math.floor(Math.random() * 16777215).toString(16)
+          });
+        }
+        // Persist URL for which activation is done (so that the activation call will only fire again after an address change)
+        last_activated_url = window.location.href;
       });
     }
   }
@@ -27,7 +37,7 @@
       observer.observe(doc.documentElement, {
         childList: true,
         subtree: true
-      });
+      }); 
     }
   }
   // Tracking script usage
